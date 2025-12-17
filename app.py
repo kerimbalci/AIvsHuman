2b31143e1690c143f8d1a7ec7ca33e13d685afb7
Kerim <okan.blc14@gmail.com>
2025-12-17 19:15:10 +0300

    Add requirements

--- app.py
+++ app.py
@@ -68,5 +68,7 @@ def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Railway'in verdiği PORT'u kullan, yoksa 5000'de çalış
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
